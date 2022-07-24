c094db9c8a875c3062c89a96b0cc069e8721dd9a
Oluwatosin Omokurolue <tosinomokurolue@gmail.com>
2022-07-24 18:48:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import "./styles.css";
+import React, { useState } from "react";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import apple from "./assets/image3.jpeg";
+import pear from "./assets/image4.jpeg";
+import avocado from "./assets/image5.jpeg";
+import orange from "./assets/image6.jpeg";
+
+const images = [cabbage, mango, apple, pear, avocado, orange];
+
+const Loading = ({ calculatedWidth }) => (
+  <aside>
+    <div className="Loading-bar">
+      <label htmlFor="images-loaded">Loading all your favorite images...</label>
+      <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
+    </div>
+  </aside>
+);
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [numLoaded, setNumLoaded] = useState(0);
+  const handleClick = () => {
+    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  const handleImageLoad = () => {
+    setNumLoaded((numLoaded) => numLoaded + 1);
+  };
+
+  return (
+    <section>
+      <header>
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fielding
+        </h2>
+      </header>
+
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+        )}
+
+        <figcaption>
+          {" "}
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => (
+          <img
+            alt=""
+            key={imageURL}
+            src={imageURL}
+            onClick={handleClick}
+            onLoad={handleImageLoad}
+            className={currentImage === index ? "display" : "hide"}
+          />
+        ))}
+      </figure>
+    </section>
+  );
+};
+
+export default App;
